7bc2b9b156b15d12ca3dae2b51d4e5eee3f13c53
abhishekvispute <abhivispute33@gmail.com>
2020-12-24 17:43:35 +0530

    testing CI by doing dummy change

--- truffle-config.js
+++ truffle-config.js
@@ -34,7 +34,7 @@ module.exports = {
      * defaults web3 uses to send transactions. If you don't specify one truffle
      * will spin up a development blockchain for you on port 9545 when you
      * run `develop` or `test`. You can ask a truffle command to use a specific
-     * network from the command line, e.g
+     * network from the command line,e.g
      *
      * $ truffle test --network <network-name>
      */
